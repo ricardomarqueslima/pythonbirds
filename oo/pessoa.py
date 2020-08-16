271280dcdb080b647375966a995360e091d4ecda
ricardomarqueslima <ricardo.marqueslima@gmail.com>
2020-08-15 23:05:38 -0400

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,11 @@
-class pessoa:
-    pass
+class Pessoa:
+    def cumprimento(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimento(p))
+    print(id(p))
+    print(p.cumprimento())
+
+
