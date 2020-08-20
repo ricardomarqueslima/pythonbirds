6c1b6957932ff76b23e6b94abc170dd50138037a
ricardomarqueslima <ricardo.marqueslima@gmail.com>
2020-08-19 22:03:07 -0400

    Criado @staticmethod e @classmethod.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,6 +10,14 @@ class Pessoa:
     def cumprimento(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     manuel = Pessoa(nome='Manuel')
     ricardo = Pessoa(manuel, nome='Ricardo')
@@ -32,7 +40,8 @@ if __name__ == '__main__':
     print(ricardo.olhos)
     print(manuel.olhos)
     print(id(Pessoa.olhos), id(ricardo.olhos), id(manuel.olhos))
-
+    print(Pessoa.metodo_estatico(), manuel.metodo_estatico(), ricardo.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), ricardo.nome_e_atributos_de_classe(), manuel.nome_e_atributos_de_classe())
 
 
 
