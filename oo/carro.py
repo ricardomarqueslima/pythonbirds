46a42c178c557a15ffe3189ba9429b6fbfac5b1b
ricardomarqueslima <ricardo.marqueslima@gmail.com>
2020-08-20 22:35:54 -0400

    Implementado motor, acelear e girar_a_direita e girar_a_esquerda

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,4 @@
+
 '''
 Você deve criar uma classe carror que vai possui dois atributos compostos por outras duas classes:
 
@@ -88,7 +89,39 @@ O    L
    >>> 'Norte'
    >>> carro.girar_a_esquerda()
    >>> carro.calcular_direcao()
-   >>> 'Oeste'E
-
-
+   >>> 'Oeste'
 '''
+
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
+class Direcao:
+
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE:SUL, SUL: OESTE, OESTE: NORTE
+    }
+
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, LESTE: NORTE, SUL: LESTE, OESTE: SUL
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+class Motor:
+    def __init__(self):
+        self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
+
+
